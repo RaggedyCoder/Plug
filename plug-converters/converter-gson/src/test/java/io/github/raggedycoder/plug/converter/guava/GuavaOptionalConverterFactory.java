68fbca00cad0820db587922610ea8359bba4c2a6
Md. Sajid Shahriar <sajid.shahriar@ipay.com.bd>
2018-10-25 12:41:52 +0600

    Added GuavaOptionalConverter Factory Template Class

---
+++ plug-converters/converter-gson/src/test/java/io/github/raggedycoder/plug/converter/guava/GuavaOptionalConverterFactory.java
@@ -0,0 +1,6 @@
+package io.github.raggedycoder.plug.converter.guava;
+
+import io.github.raggedycoder.plug.converter.PlugConverter;
+
+public class GuavaOptionalConverterFactory extends PlugConverter.Factory{
+}
